d171ec4601a75c026bba871842d702d92e3ba39c
dcbentley <danielcbentley@gmail.com>
2020-07-12 18:48:56 -0400

    added target button fixed api fetch

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
 // Target the control-panel buttons
-let controlPanelButtons = document.querySelector('.start-reset-button');
+let controlPanelButton = document.querySelector('.js-button');
 
 // Add event listener to the control-panel start-reset button
-controlPanelButtons.addEventListener('click', handleStartReset);
+controlPanelButton.addEventListener('click', handleStartReset);
 
-// Create short category drop down list
-// let catList = document.createElement('');
+// [TODO] Create short category dropdown list and store to variable for url
+// [TODO] Create number of questions dropdown list and store to variable for url
+// [TODO] Create start button in DOM
+// let catStart = document.createElement('button');
 
 
 // Connect to trivia API for questions
 function handleStartReset (e) {
-    // let userInput = document.querySelector('')
-    e.preventDefaults();
     console.log({e})
     // Fetch category URL Variable and number of questions
-    let catURL = e.target.dataset.url;  // THIS REQUIRES ATTENTION
-    let numbOfQuestions = e.target.dataset.questions; // THIS REQUIRES ATTENTION
+    // let catURL = e.target.dataset.url;  // THIS REQUIRES ATTENTION
+    // let numbOfQuestions = e.target.dataset.questions; // THIS REQUIRES ATTENTION
     
     fetch ('https://opentdb.com/api.php?amount=10&category=18&type=multiple')
             .then((res) => {
@@ -25,4 +25,7 @@ function handleStartReset (e) {
                 // Check res output
                 console.log(res);
             })
+            if (event.target.classList.contains('.js-button')){
+                console.log("Line 30")
+            };
 }
