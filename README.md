c2cbef4889a740e3f8d115b6c572fde71bc15440
dcbentley <danielcbentley@gmail.com>
2020-07-16 00:12:56 -0400

    added to readme and comments to js

--- README.md
+++ README.md
@@ -15,11 +15,16 @@ List of Technologies Used:
 * CSS
 * JavaScript
 * Open Trivia db API
+* fetch
 
 Challenges:
 * DOM manipulation can be a little bit tricky. The added complexity of working with an API also ate up more time to track down bugs in how I'd implemented the game design.
 
+Unsolved problems and known bugs
+* The questionsArray is being called before it's expected and duplicate answers are added to the game after the second question.
+*  Some questions from the Trivia API have HTML characters and I believe encoding them as base64 and then running through atob() function to decode would render the text as expected. (might also be something that can be added to the URL)
+
 Additional Enhancements:
 * I'd like to add a game modal box that ask the player if they'd like to play a new game after answering all the questions. 
 * I'd like to add a drop down so the player can select the number of questions and have that displayed
-* Add game play countdown timer
+* Add game play countdown timer and time based scoring
--- app.js
+++ app.js
@@ -6,39 +6,43 @@ let scoreTotal = 0;
 // ---------------
 // QUESTIONS ARRAY
 // ---------------
-let questionsArray = [];
-let currentQuestion = 0;
-let answersArray;
-let currentAnswer = null;
-let haveQuestions = false;
+let questionsArray = [];					// Empty questionsArray
+let currentQuestion = 0;					// Incrementor for questionsArray
+let answersArray = [];						// Array for all possible answers
+let currentAnswer = null;					// The correct answer to the question				
+let haveQuestions = false;					// Logic piece for if/else GAME Start
 
-// Connect to trivia API for questions
-// API information fetch
+// --------------------------------------------------
+// CONNECT TO Open Trivia API - API information fetch
+// --------------------------------------------------
 function getQuestions (){
 	fetch('https://opentdb.com/api.php?amount=10&category=18&type=multiple')
-	.then((res) => {
-		return res.json();
+	.then((res) => {						// Promise
+		return res.json();					// json results
 	})
-		.then((res) => {
+		.then((res) => {					// Promise
+			// -----------------------
 			// SEND TO QUESTIONS ARRAY
-			questionsArray = res.results;
-			haveQuestions = true;
-			handleStartReset();
+			// -----------------------
+			questionsArray = res.results;	// Question results from Trivia API
+			haveQuestions = true;			// Logic if/else piece for GAME START
+			handleStartReset();				// Invoke GAME START
 		})
 	}
-
+// ----------
+// GAME START
+// ----------
 function handleStartReset() {
-	if (haveQuestions) {	
+	if (haveQuestions) {	// If haveQuestions is false goto else that invokes getQuestions
 		// ---------------------------------
 		// CREATE RANDOM ORDER FOR QUESTIONS
 		// ---------------------------------
-		// Button order including answer button
 		let answersArrayCorrectIndex = Math.floor(Math.random() * 4);
 	
 		// --------------------------------
 		// PUSH ALL ANSWERS TO ARRAY
 		// --------------------------------
-		// answerArray storing the incorrect answers
+		// Take all incorrect answers and add to answersArray
 		answersArray = questionsArray[currentQuestion].incorrect_answers;
 			
 		// Selecting the correct answer and storing it to currentAnswer
@@ -51,16 +55,16 @@ function handleStartReset() {
 		// ------------------------------
 		// DOM and question added to HTML
 		// ------------------------------
+		// Target HTML class
 		let questionTargetLocation = document.querySelector('.questions-box');
-
-		// Confirm questionTargetLocation
+		// Create p element to hold questions
 		let createQuestionDisplayElement = document.createElement('p');
+		// Add additional class to element to help in targeting
 		createQuestionDisplayElement.setAttribute('class', 'question');
-
-		// Confirm createQuestionDisplayElement
+		// Append created element to the question-box
 		questionTargetLocation.append(createQuestionDisplayElement);
+		// P element text from the current question
 		createQuestionDisplayElement.innerText = questionsArray[currentQuestion].question;
-		
 		// DOM create buttons and add innerTEXT for each
 		let answerTargetLocation = document.querySelector('.js-answer-buttons');
 			
@@ -73,17 +77,16 @@ function handleStartReset() {
 			// CREATE BUTTONS
 			// --------------
 			let newButton = document.createElement('button');
-
-			// Fill it with some content
+			// Fill it with answersArray content and iterate
 			let newButtonText = document.createTextNode(`${answersArray[a]}`);
 			newButton.appendChild(newButtonText);
-
 			// Add text node to the newly created buttons
 			answerTargetLocation.appendChild(newButton);
-
 			// Set data-attribute that this is the correct answer to buttons
 			newButton.setAttribute('data-id', a);
+			// Set class of answer to each button for propagation 
 			newButton.setAttribute('class', 'answer');
+			// Click handler for newButton aka question answers
 			newButton.addEventListener('click', answersHandler);
 		}
 		
@@ -93,55 +96,70 @@ function handleStartReset() {
 		function answersHandler(e) {
 			// Assign data-id attribute
 			let placeHolder = e.target.getAttribute('data-id');
-			
-			// Get player score display
+			// Target class item for player score
 			let playerScore = document.querySelector('.player-score');
+			// Get elements with the class of answers - prep to disable button after click
 			let buttons = document.getElementsByClassName('answer');
+			
+			// ------------------------------------------
+			// LOOP TO DISABLE ANSWER BUTTONS AFTER CLICK
+			// ------------------------------------------
 			for (let i = 0; i < buttons.length; i++){
 				buttons[i].disabled = true;
 			};
 			
-			// Logic test to see if the random that we generated matches our stored value and score
-			if (answersArrayCorrectIndex == placeHolder) {
-				scoreTotal += 10;
-				playerScore.innerText = scoreTotal;
-				e.target.style.color = 'green';
-				e.target.style.fontWeight = "900";
-			} else {
-				scoreTotal -= 5;
-				playerScore.innerText = scoreTotal;
-				e.target.style.color = 'red';
-				e.target.style.fontWeight = '900';
+			// Logic to validate if button selected matched the random index number assigned to our correct answer
+			if (answersArrayCorrectIndex == placeHolder) {	// Match comparison
+				scoreTotal += 10;							// Scoring
+				playerScore.innerText = scoreTotal;			// Update scoring
+				e.target.style.color = 'green';				// Correct style selection color
+				e.target.style.fontWeight = "900";			// Increasing font boldness
+			} else {										// If not a match
+				scoreTotal -= 5;							// Subtract points
+				playerScore.innerText = scoreTotal;			// Update scoring
+				e.target.style.color = 'red';				// Incorrect style selection color
+				e.target.style.fontWeight = '900';			// Increasing font boldness
 			}
 			
 			// Create Next Question Button to load additional questions
 			let nextQuestionButton = document.createElement('button');
+			// Create TextNode for button
 			let nextQuestionText = document.createTextNode('Next Question');
+			// Append TextNode to nextQuestionButton
 			nextQuestionButton.append(nextQuestionText);
+			// Target class item to place button
 			let nextQuestionControl = document.querySelector('.game-nav-controls');
+			// Append to button
 			nextQuestionControl.append(nextQuestionButton);
+			// Add class to button
 			nextQuestionButton.setAttribute('class', 'next-button');
-			// ---------------------
-			// Next Question advance
-			// ---------------------
+			
+			// -----------------------------------
+			// NEXT QUESTION ADVANCE CLICK HANDLER
+			// -----------------------------------
 			nextQuestionControl.addEventListener('click', clearGameBox);
+			
+			// -------------------------
 			// INCREMENT QUESTION NUMBER
+			// -------------------------
 			currentQuestion++;
 		}
 		// ----------------------------------------
-		// Clearing the Game Box for a new question
+		// CLEARING THE GAME BOX FOR A NEW QUESTION
 		// ----------------------------------------
 		function clearGameBox() {
+			// Target different classes so we can clear them
 			let question = document.querySelector('.questions-box');
 			let answerButtons = document.querySelector('.answer-buttons');
 			let clearGameNavControls = document.querySelector('.game-nav-controls');
+			// Clear the previously targeted elements and replace with empty strings
 			question.innerHTML = '';
 			answerButtons.innerHTML = '';
 			clearGameNavControls.innerHTML = '';
-			handleStartReset();
+			handleStartReset();							// Invoke GAME START
 		}
 	} else {
-		getQuestions();
+		getQuestions();									// exit if loop - API fetch
 	};
 };
-handleStartReset();
+handleStartReset();	// Runs after loading JS then loops through if / else to before calling API
